4a78122b5367568e80561ad0204e613e57c3963d
celguar <celguar@gmail.com>
2020-09-05 03:30:30 +0300

    Speed up shaman healing

--- playerbot/strategy/shaman/GenericShamanStrategy.cpp
+++ playerbot/strategy/shaman/GenericShamanStrategy.cpp
@@ -117,23 +117,23 @@ void GenericShamanStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
 
 	triggers.push_back(new TriggerNode(
         "party member medium health",
-		NextAction::array(0, new NextAction("lesser healing wave on party", 25.0f), NULL)));
+		NextAction::array(0, new NextAction("lesser healing wave on party", 20.0f), NULL)));
 
 	triggers.push_back(new TriggerNode(
         "party member low health",
-		NextAction::array(0, new NextAction("riptide on party", 25.0f), NULL)));
+		NextAction::array(0, new NextAction("riptide on party", 50.0f), NULL)));
 
 	triggers.push_back(new TriggerNode(
 		"medium aoe heal",
-		NextAction::array(0, new NextAction("chain heal", 27.0f), NULL)));
+		NextAction::array(0, new NextAction("chain heal", 40.0f), NULL)));
 
 	triggers.push_back(new TriggerNode(
 		"medium health",
-		NextAction::array(0, new NextAction("lesser healing wave", 26.0f), NULL)));
+		NextAction::array(0, new NextAction("lesser healing wave", 25.0f), NULL)));
 
 	triggers.push_back(new TriggerNode(
 		"low health",
-		NextAction::array(0, new NextAction("riptide", 26.0f), NULL)));
+		NextAction::array(0, new NextAction("riptide", 60.0f), NULL)));
 
 	triggers.push_back(new TriggerNode(
 		"heroism",
